0d7cdc823b12b0f15aec582b605b78998f1e8c86
yfu03 <yannisfu@gmail.com>
2023-06-17 18:48:55 -0500

    auto generating json data files

---
+++ src/main/java/net/yf/tutorialmod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,30 @@
+package net.yf.tutorialmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.yf.tutorialmod.TutorialMod;
+import net.yf.tutorialmod.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper)  {
+        super(output, TutorialMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.BLACK_OPAL_BLOCK);
+        blockWithItem(ModBlocks.BLACK_OPAL_ORE);
+        blockWithItem(ModBlocks.DEEPSLATE_BLACK_OPAL_ORE);
+        blockWithItem(ModBlocks.NETHERRACK_BLACK_OPAL_ORE);
+        blockWithItem(ModBlocks.ENDSTONE_BLACK_OPAL_ORE);
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+
+
+}
---
+++ src/main/java/net/yf/tutorialmod/datagen/ModLootTableProvider.java
@@ -0,0 +1,17 @@
+package net.yf.tutorialmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+
+
+import java.util.List;
+import java.util.Set;
+
+public class ModLootTableProvider {
+    public static LootTableProvider create(PackOutput output) {
+        return new LootTableProvider(output, Set.of(),
+                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)));
+                //list of providers can be extended with other things like chests, fishing, etc
+    }
+}
